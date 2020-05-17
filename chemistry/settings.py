40dfe6bca1d9a6c572b65c72d95bda78f15e23fa
driveru <promitej.fadeev30@mail.ru>
2020-05-17 09:09:38 +0300

    Added files and changes required for development to heroku

--- chemistry/settings.py
+++ chemistry/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +131,9 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/knowledge'
 LOGOUT_REDIRECT_URL = '/knowledge'
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
