806abf36144b8782e3ef9a6248c426bf0771c798
driveru <promitej.fadeev30@mail.ru>
2020-05-26 16:34:25 +0300

    Adding home page

--- home/views.py
+++ home/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 from knowledge.views import is_allowed
 # Create your views here.
 def index(request):
+    context = {}
+    if is_allowed(request.user.groups.all()):
+        context = {'allowed': True}
     return render(request, 'home/index.html')
 
 def about(request):
+    context = {}
     if is_allowed(request.user.groups.all()):
         context = {'allowed': True}
     return render(request, 'home/about.html', context)
